76ba334b6f56d30c43b619f1c650e56af6ddf5cd
Divyanshu <divyanshutiwari173@gmail.com>
2025-11-27 23:38:49 +0530

    completed newsletter.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
     <Categories/>
     <BestSeller/>
     <BottomBanner/>
+    <NewsLetter/>
     </div>
   )
 }
